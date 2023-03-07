6471c29145732756c1e96e4901a1568a5621f355
colio-19 <126813004+colio-19@users.noreply.github.com>
2023-03-07 13:22:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    lazer = game.createSprite(shooter.get(LedSpriteProperty.X), shooter)
+})
+let lazer: game.LedSprite = null
+let shooter: game.LedSprite = null
+shooter = game.createSprite(2, 5)
+basic.forever(function () {
+    shooter.move(1)
+    basic.pause(150)
+    shooter.ifOnEdgeBounce()
+})
